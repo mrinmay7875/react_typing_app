faf046d5b6d77f84b18bedb28ec5ce8773b84fa7
Mrinmay Santra <mrinmay7875@gmail.com>
2024-07-06 14:46:29 +0530

    updated tailwind config file after removing nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
-// tailwind.config.js
-// const { nextui } = require('@nextui-org/react');
-// eslint-disable-next-line no-undef
-const { nextui } = require('@nextui-org/react');
-
-const config = {
-  content: [
-    // ...
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  darkMode: 'class',
-  plugins: [nextui()],
+  plugins: [],
 };
-
-export default config;
