c48b68d4f0c6f1021d7a19036e5f65fdd0f1b914
Mrinmay Santra <mrinmay7875@gmail.com>
2024-07-06 21:16:02 +0530

    Created Play Again Button

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -15,8 +15,14 @@ function Result() {
           <span className='text-teal-300'>{` ${calculateScore()} `}</span>
           words per minute
         </h2>
-        <h3 className='text-lg text-gray-500'>
-          Refresh the page to play again!
+        <h3 className='text-lg text-gray-500 mt-5'>
+          <button
+            type='button'
+            onClick={() => window.location.reload()}
+            className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+          >
+            Play Again
+          </button>
         </h3>
       </div>
     );
